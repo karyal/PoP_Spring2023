10287b15bc4e90bcc3757d07278c447e960ee48f
= <=>
2023-03-10 11:22:23 +0545

    March 10 2023

---
+++ PoP_C/src/week8/day2/GUI.java
@@ -0,0 +1,5 @@
+package week8.day2;
+
+public class GUI {
+
+}
---
+++ PoP_C/src/week8/day2/JDBC.java
@@ -0,0 +1,63 @@
+package week8.day2;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+public class JDBC {
+	public boolean insert(int pid, String name) {
+		//sql statement
+		String sql = "INSERT INTO tbl_persons VALUES(?, ?);";
+		try {
+			//connect
+			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/level4c","root", "");
+			//prepared statement
+			PreparedStatement pstat = conn.prepareStatement(sql);
+			//set values
+			pstat.setInt(1, pid);
+			pstat.setString(2, name);
+			//execute sql statement
+			pstat.executeUpdate(); //insert record
+			//close all
+			pstat.close();
+			conn.close();
+		} catch (Exception ex) {
+			//error message
+		}
+		return false;//return result
+	}
+
+	public boolean update(int pid, String name) {
+		try {
+
+		} catch (Exception ex) {
+
+		}
+		return false;
+	}
+
+	public void all() {
+		try {
+
+		} catch (Exception ex) {
+
+		}
+	}
+
+	public void search(int pid) {
+		try {
+
+		} catch (Exception ex) {
+
+		}
+	}
+
+	public boolean delete(int pid) {
+		try {
+
+		} catch (Exception ex) {
+
+		}
+		return false;
+	}
+}
