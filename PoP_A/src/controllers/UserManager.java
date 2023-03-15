5ab3aa50495de2ee25c8ec2feca937fd1614bdeb
= <=>
2023-03-15 14:38:51 +0545

    March 15 2023

---
+++ PoP_A/src/controllers/UserManager.java
@@ -0,0 +1,15 @@
+package controllers;
+import models.User;
+
+public class UserManager {
+	public boolean login(User user) {
+		//Receive user info from UI
+		//Validate user info
+		//Send user info to DB
+		//Receive user status from DB
+		//Return Login status to UI
+		UserJDBC userJDBC = new UserJDBC();
+		boolean result = userJDBC.login(user);
+		return result;
+	}
+}
--- PoP_C/src/week9/day2/ManageLogin.java
+++ PoP_C/src/week9/day2/ManageLogin.java
@@ -2,6 +2,10 @@ package week9.day2;
 
 public class ManageLogin {
 	public User login(User user) {
+		//receive user info from UI
+		//send user info to JDBC class
+		//receive login status from JDBC class
+		//return login status to UI
 		return new User();
 	}
 }
